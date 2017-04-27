d20d66701d98e1bf915e13fcdda40f2a1adfae34
Scott O'Neil <ScottO@Scotts-MacBook-Pro.local>
2017-04-27 14:58:22 -0400

    init commit

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Scott's Resume</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+  </head>
+  <body>
+
+<div id="wrapper">
+    <div class ="row" id = "theNav">
+    <span class = "col-md-3 text-center"> HOME
+    </span>
+    <span class = "col-md-3 text-center"> ABOUT
+    </span>
+    <span class = "col-md-3 text-center"> MY WORK
+    </span>
+    <span class = "col-md-3 text-center"> CONTACT
+    </span>
+    </div>
+</div>
---
+++ index.php
@@ -0,0 +1,6 @@
+
+
+<?php include 'header.php';?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  </body>
+</html>
